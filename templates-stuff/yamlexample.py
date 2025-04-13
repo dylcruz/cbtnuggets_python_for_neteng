9ce744964fb9114884323dfc294b00d57f7db3fc
dylcruz <dylan@dylancruz.me>
2025-04-13 16:12:43 -0700

    jinja2 and templating examples

---
+++ templates-stuff/yamlexample.py
@@ -0,0 +1,17 @@
+import yaml
+from rich import print as rprint
+
+def pull_yaml():
+    """
+    Function to pull yaml data
+    """
+    config_data = yaml.safe_load(open('R1.yaml'))
+    rprint(config_data)
+    bgp_stuff = config_data['BGP']
+    peers = bgp_stuff['peers']
+    for peer in peers:
+        nbor = peer['neighbor']
+        asnumber = peer['peer_asn']
+        print(f'The neighbor {nbor} has an ASN of {asnumber}.')
+
+pull_yaml()
